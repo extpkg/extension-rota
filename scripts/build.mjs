8951231d9cbe4992936e4e3b064a9aba2cdbb5cd
Bogdan B Peterson <github@bogdanbpeterson.com>
2023-12-26 19:59:07 +0200

    ci: adds zip file generation

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 
+import { execSync } from "node:child_process";
 import * as esbuild from "esbuild";
 import {
   clean,
@@ -22,3 +23,14 @@ export const build = async () => {
 };
 
 await build();
+
+if (process.env.NODE_ENV === "production") {
+  try {
+    execSync("(rm -f dist.zip && cd dist && zip -r ../dist.zip .)", {
+      stdio: "inherit",
+    });
+  } catch (error) {
+    console.error("Error running npm run build:", error);
+    process.exit(1);
+  }
+}
